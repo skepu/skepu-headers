2f64106610c9b14fd96de25d7b4a819398ef27ae
Johan Ahlqvist <johan.ahlqvist@liu.se>
2021-05-07 12:28:04 +0200

    [StarPU-MPI] Fixed an issue with static skeletons in global scope.

--- src/skepu3/cluster/cluster.hpp
+++ src/skepu3/cluster/cluster.hpp
@@ -1,6 +1,8 @@
 #ifndef CLUSTER_HPP
 #define CLUSTER_HPP
 
+#include <memory>
+
 #include <starpu.h>
 #include <starpu_mpi.h>
 
@@ -13,7 +15,6 @@ namespace skepu {
 				int mpi_size;
 				int mpi_tag {1};
 
-				// Figure out howto enable all cores when not using STARPU_NCPU
 				internal_state() {
 					starpu_conf_init(&conf);
 					conf.sched_policy_name = "peager";
@@ -23,10 +24,6 @@ namespace skepu {
 					{
 						conf.single_combined_worker = 1;
 					}
-					// Not using starpu_mpi_init_conf because that makes
-					// starpu_mpi_shutdown segfault.
-					// TODO:As the performance models in the skeletons are not static in
-					// any way. Change that and it shall work fine.
 					auto err = starpu_init(&conf);
 					assert(!err);
 					starpu_mpi_init(NULL, NULL, 1);
@@ -37,8 +34,6 @@ namespace skepu {
 
 				~internal_state() {
 					starpu_mpi_shutdown();
-					// TODO: See comment in constructor...
-					//starpu_mpi_shutdown();
 				};
 			};
 		}
@@ -49,52 +44,59 @@ namespace skepu {
 	namespace cluster {
 		namespace state {
 
-			inline internal_state & s() {
-				static internal_state g_state;
-				return g_state;
+			inline internal_state * s() {
+				bool static initialized{true};
+				auto deleter = [&](internal_state * ptr) {
+					initialized = false;
+					delete ptr;
+				};
+				std::unique_ptr<internal_state, decltype(deleter)> static g_state{
+					new internal_state,
+					deleter};
+
+				if(initialized)
+					return g_state.get();
+				else
+					return 0;
 			}
 		}
 
 		static size_t
 		mpi_rank() {
-			return state::s().mpi_rank;
+			return state::s()->mpi_rank;
 		};
 
 		static size_t
 		mpi_size() {
-			return state::s().mpi_size;
+			return state::s()->mpi_size;
 		};
 
-		// Return a new mpi_tag for use with a handle, as a unique
-		// identifier for use in registering data handles. This function
-		// *must* be called coherently across all ranks.
+		/** Return a new mpi_tag for use with a handle, as a unique identifier for
+		 * use in registering data handles.
+		 *
+		 * This function *must* be called coherently across all ranks.
+		 */
 		static size_t
 		mpi_tag() {
-			return state::s().mpi_tag++;
+			return state::s()->mpi_tag++;
 		};
 
 		static auto
 		starpu_ncpus()
 		-> int
 		{
-			return state::s().conf.ncpus;
+			return state::s()->conf.ncpus;
 		}
 
 		static auto
 		barrier()
 		-> void
 		{
-			if(starpu_is_initialized())
+			if(state::s())
 			{
 				starpu_mpi_wait_for_all(MPI_COMM_WORLD);
 				starpu_mpi_barrier(MPI_COMM_WORLD);
 			}
-		}
-	}
-
-	inline void wait_for_all_tasks() {
-		if(starpu_is_initialized() && starpu_task_nsubmitted()) {
-			starpu_task_wait_for_all();
 		}
 	}
 }
